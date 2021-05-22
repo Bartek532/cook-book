a1e4692536f15ef939e69a924af1fb8bc9c1da91
Bartek532 <57185551+Bartek532@users.noreply.github.com>
2021-05-22 15:10:02 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <Hero />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Hero from "./views/Hero.vue";
+
+export default defineComponent({
+  name: "App",
+  components: {
+    Hero,
+  },
+});
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/views/Hero.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Hello World</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Hero",
+});
+</script>
+
+<style lang="scss" scoped></style>
